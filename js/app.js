14e074a47c79236c8f77073cbac3f5441fc9555d
Goakiller96 <71164390+Goakiller96@users.noreply.github.com>
2025-06-20 16:11:57 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -368,7 +368,7 @@
         document.addEventListener("click", documentActions);
         function documentActions(e) {
             const targetElement = e.target;
-            if (window.innerWidth > 768 && isMobile.any()) if (targetElement.classList.contains("menu__arrow")) targetElement.closest(".menu__item").classList.toggle("_hover");
+            if (window.innerWidth > 768.98 && isMobile.any()) if (targetElement.classList.contains("menu__arrow")) targetElement.closest(".menu__item").classList.toggle("_hover");
             if (targetElement.classList.contains("search-form__icon")) document.querySelector(".search-form").classList.toggle("_active"); else if (!targetElement.closest(".search-form") && document.querySelector(".search-form._active")) document.querySelector(".search-form").classList.remove("_active");
         }
         const headerElement = document.querySelector(".header");
